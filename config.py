042f48bd19dcbeedc6f7c4bb28cefd70e581f37a
Rodolfo <rodolfo.diaz@nokia.com>
2022-02-20 18:54:18 -0500

    migrate & postgresql

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'postgresql://postgres:postgres@localhost/microblog'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'postgresql://postgres:nemuuser@localhost:5432/microblog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
