621c0ca13a85b8a1fa091355e2b8eebac05c4af2
Rodolfo <rodolfo.diaz@nokia.com>
2022-02-20 21:38:01 -0500

    foreing key to posts

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from app import db
+from sqlalchemy import DateTime,Integer
+from datetime import datetime
 
 
 class User(db.Model):
@@ -6,6 +8,16 @@ class User(db.Model):
     username = db.Column(db.String(64),index=True,unique= True)
     email = db.Column(db.String(120),index=True,unique=True)
     password_hash = db.Column(db.String(128))
-    
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
     def __repr__(self) -> str:
         return f'<User {self.username}>'
+
+class Post(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp=db.Column(DateTime(),index=True,default=datetime.utcnow)
+    user_id = db.Column(Integer(),db.ForeignKey('user.id'),nullable=False)
+
+    def __repr__(self) -> str:
+        return f'<Post {self.body}>'
