3c413093a54f7996e2d4aa6551452df3e97e716d
Rodolfo <rodolfo.diaz@nokia.com>
2022-02-20 19:07:34 -0500

    securing

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'postgresql://postgres:nemuuser@localhost:5432/microblog'
+    SECRET_KEY = os.environ.get('SECRET_KEY') #or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') #or 'postgresql://postgres:nemuuser@localhost:5432/microblog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
