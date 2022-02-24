351bd90c4f734fda4980f0bb726d0daac7ce4461
Rodolfo <abelardo.diaz@pucp.pe>
2022-02-24 15:10:57 -0500

    update config

--- config.py
+++ config.py
@@ -2,9 +2,7 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URI') or \
-        'postgresql://postgres:nemuuser@localhost:5432/microblog'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POST_PER_PAGE = 25
