f8e0017c6e1ff81ecb5191ca3c491a48f60ae763
rodolfo <abelardo.diaz@pucp.pe>
2022-02-25 21:06:30 -0500

    progress bar to job in redis

--- app/tasks.py
+++ app/tasks.py
@@ -1,10 +1,19 @@
 from time import sleep
+from rq import get_current_job
+
 def example(seconds):
-    print('starting task')
-    for i in range(seconds):
-        print(i)
-        sleep(1)
-    print('task completed')
+    job = get_current_job()
+    if job:
+        job.meta['progress'] = 0
+        print('starting task')
+        for i in range(seconds):
+            print(i)
+            job.meta['progess'] = 100 * i / seconds
+            job.save_meta()
+            sleep(1)
+        job.meta['progress'] = 100
+        job.save_meta()
+        print('task completed')
 
 if __name__ == '__main__':
     example(3)
