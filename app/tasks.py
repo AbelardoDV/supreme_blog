f4f2dcdc393774aa1ae3f4dc7a5785ab193becb5
rodolfo <abelardo.diaz@pucp.pe>
2022-02-25 21:10:24 -0500

    reach 100 job in redis server

--- app/tasks.py
+++ app/tasks.py
@@ -3,17 +3,17 @@ from rq import get_current_job
 
 def example(seconds):
     job = get_current_job()
-    if job:
-        job.meta['progress'] = 0
-        print('starting task')
-        for i in range(seconds):
-            print(i)
-            job.meta['progess'] = 100 * i / seconds
-            job.save_meta()
-            sleep(1)
-        job.meta['progress'] = 100
+
+    job.meta['progress'] = 0
+    print('starting task')
+    for i in range(seconds):
+        print(i)
+        job.meta['progess'] = 100 * i / seconds
         job.save_meta()
-        print('task completed')
+        sleep(1)
+    job.meta['progress'] = 100
+    job.save_meta()
+    print('task completed')
 
 if __name__ == '__main__':
     example(3)
